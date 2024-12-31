1d5869e363e44711088c0bc71c8dd9c173000950
FaberJoo <sjdk0805@gmail.com>
2024-12-31 21:49:23 +0900

    refactor: style 중복 제거 #2

--- src/components/Layout/Header.styles.jsx
+++ src/components/Layout/Header.styles.jsx
@@ -1,8 +1,6 @@
 export const getHeaderStyles = (isMobile) => ({
   height: '55px',
-  border_bottom: '1px solid #eee',
   display: 'flex',
-  align_items: 'center',
   padding: '0 12px',
   justifyContent: isMobile ? 'center' : 'normal',
   borderBottom: '1px solid #eee',
