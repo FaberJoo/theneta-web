4cc0dff5e0223831a08b20a45175e6c5a01a384c
FaberJoo <sjdk0805@gmail.com>
2025-01-01 21:18:56 +0900

    feat: drawer 컴포넌트 추가 #10

--- src/components/Layout/AppShell.jsx
+++ src/components/Layout/AppShell.jsx
@@ -1,33 +1,43 @@
-import { AppShell as MantineAppShell } from '@mantine/core';
-import { useMediaQuery } from '@mantine/hooks';
-import { appShellStyles, headerStyles, navStyles } from './AppShell.styles';
+import { AppShell as MantineAppShell, Drawer } from '@mantine/core';
+import { useMediaQuery, useDisclosure } from '@mantine/hooks';
 import PropTypes from 'prop-types';
 import Header from './Header';
 import Nav from './Nav';
+import { appShellStyles } from './AppShell.styles';
 
 export default function AppShell({ children }) {
   const isMobile = useMediaQuery('(max-width: 920px)');
+  const [opened, { open, close }] = useDisclosure(false);
 
   return (
-    <MantineAppShell
-      header={headerStyles(isMobile)}
-      navbar={navStyles(isMobile)}
-      padding='md'
-      layout='default'
-      styles={appShellStyles(isMobile)}
-    >
-      <MantineAppShell.Header withBorder>
-        <Header />
-      </MantineAppShell.Header>
-
-      <div style={{ display: 'flex', width: '100%' }}>
-        <MantineAppShell.Navbar withBorder>
-          <Nav />
-        </MantineAppShell.Navbar>
-
+    <>
+      <MantineAppShell styles={appShellStyles(isMobile)}>
+        <Header onOpenDrawer={open} />
+        <Nav />
         <MantineAppShell.Main>{children}</MantineAppShell.Main>
-      </div>
-    </MantineAppShell>
+      </MantineAppShell>
+
+      <Drawer.Root
+        opened={opened}
+        onClose={close}
+        position='right'
+        style={{
+          position: 'fixed',
+          top: 0,
+          zIndex: 1001,
+          backgroundColor: 'white',
+        }}
+      >
+        <Drawer.Overlay />
+        <Drawer.Content>
+          <Drawer.Header>
+            <Drawer.Title>Drawer title</Drawer.Title>
+            <Drawer.CloseButton />
+          </Drawer.Header>
+          <Drawer.Body>Drawer content</Drawer.Body>
+        </Drawer.Content>
+      </Drawer.Root>
+    </>
   );
 }
 
